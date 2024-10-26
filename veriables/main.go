3db2e3a1b63626e00ab69cb49a7ce0d745641c5d
ashudevcodes <ashishprasad949@gmail.com>
2024-10-26 16:25:16 +0530

    veriable in go

---
+++ veriables/main.go
@@ -0,0 +1,29 @@
+package main
+
+import "fmt"
+
+func main() {
+	// string
+	var name string = "ashish"
+	// infer
+	var last_name = "prasad"
+	fmt.Println(name)
+	fmt.Println(last_name)
+	// bool
+	var isAdult = true
+	var isMale bool = true
+	fmt.Println(isAdult)
+	fmt.Println(isMale)
+
+	// shotHand Syntex
+
+	gander := "male"
+	fmt.Println(gander)
+	var age int
+
+	age = 21
+	fmt.Println(age)
+	var price float32
+	price = 34.3
+	fmt.Println(price)
+}
