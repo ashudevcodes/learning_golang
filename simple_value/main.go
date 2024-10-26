0a795977c15f76d3e2f4249ae9cab682f3fb1bdc
ashudevcodes <ashishprasad949@gmail.com>
2024-10-26 16:24:07 +0530

    simple values in go

---
+++ simple_value/main.go
@@ -0,0 +1,17 @@
+package main
+
+import "fmt"
+
+func main() {
+	// integers
+	fmt.Println(1)
+	// string
+	fmt.Println("ashish")
+	// bool
+	fmt.Println(true)
+	fmt.Println(false)
+	// floats
+	fmt.Println(10.4)
+	fmt.Println(7.0 / 3.0)
+
+}
